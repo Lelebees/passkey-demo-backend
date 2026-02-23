f08ae9ae7fa39cf63399463e324cbdf88241b9ee
Lelebees <Lelebees@protonmail.com>
2026-02-23 15:52:51 +0100

    erm whoops

--- src/main/java/nl/lelebees/passkeydemo/backend/application/dto/UserDto.java
+++ src/main/java/nl/lelebees/passkeydemo/backend/application/dto/UserDto.java
@@ -1,12 +1,11 @@
 package nl.lelebees.passkeydemo.backend.application.dto;
 
-import nl.lelebees.passkeydemo.backend.domain.ChallengeEntity;
 import nl.lelebees.passkeydemo.backend.domain.User;
 
 import java.util.UUID;
 
-public record UserDto(UUID id, String email, String displayName, ChallengeEntity issuedChallenge) {
+public record UserDto(UUID id, String email, String displayName) {
     public static UserDto From(User user) {
-        return new UserDto(user.getId(), user.getEmail().toString(), user.getDisplayName(), user.getIssuedChallenge());
+        return new UserDto(user.getId(), user.getEmail().toString(), user.getDisplayName());
     }
 }
